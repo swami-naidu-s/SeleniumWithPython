f01a3892a07cfcff50c3a6b88b3c6aaa77580da2
Seepana Swaminaidu <seepanaswaminaidu@gmail.com>
2026-07-07 17:36:32 +0530

    Added pipeline and failed screenshot mechanism

--- Tests/test_invoke.py
+++ Tests/test_invoke.py
@@ -1,2 +1,3 @@
 def test_case1(pages):
     print(pages.driver.title)
+    pages.driver.switch_to.alert.accept()
--- conftest.py
+++ conftest.py
@@ -1,7 +1,9 @@
+import os
 import pytest
 
 from Base.Browser import Browser
 from Base.Utilities import get_config_data
+from datetime import datetime
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.remote.webdriver import WebDriver
 
@@ -16,3 +18,17 @@ def pages():
     wait: WebDriverWait = WebDriverWait(driver, get_config_data("explicit_wait"))
     yield Pages(driver, wait)
     Browser.clean_up_browser(driver)
+
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_makereport(item):
+    outcome = yield
+    report = outcome.get_result()
+
+    if report.when == "call" and report.failed:
+        pages = item.funcargs.get("pages")
+        if pages:
+            folder: str = "screenshots"
+            os.makedirs(folder, exist_ok=True)
+            timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+            pages.driver.save_screenshot(os.path.join(folder, f"{item.name}_{timestamp}.png"))
+            print(f"Sceenshot captured with timestamp {timestamp}")

848c2fa518363cbf6fe0edfb6ef30ebe94766eab
Seepana Swaminaidu <seepanaswaminaidu@gmail.com>
2026-07-15 18:55:09 +0530

    Added reusable methods

--- Base/SeleniumBase.py
+++ Base/SeleniumBase.py
@@ -1,4 +1,5 @@
 from logging import Logger
+from time import sleep
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.remote.webelement import WebElement
@@ -85,6 +86,79 @@ class SeleniumBase:
             self.logger.exception(e)
             raise f"Exception occured: {e}"
         
+    def right_click(self, element: tuple[str, str] | WebElement, log_message: str, timeout_seconds: int | None = None):
+        try:
+            self.wait_for_element_to_be_visible(element, timeout_seconds)
+            self.scroll_to_element(element)
+            self.wait_for_element_to_be_clickable(element, timeout_seconds)
+            if isinstance(element, tuple[str, str]):
+                element = self.driver.find_element(*element)
+            ActionChains(self.driver).move_to_element(element).context_click().perform()
+            self.logger.info(f"{log_message} | Right clicked on element | {element}")
+        except Exception as e:
+            self.logger.exception(e)
+            raise f"Exception occured: {e}"
+        
+    def is_enabled(self, element: tuple[str, str] | WebElement, timeout_seconds: int | None = None) -> bool:
+        result: bool = False
+        if isinstance(element, tuple[str, str]):
+            element = self.driver.find_element(*element)
+        if isinstance(timeout_seconds, None):
+            result = element.is_enabled()
+        else:
+            while timeout_seconds > 0:
+                result = element.is_enabled()
+                if result:
+                    break
+                else:
+                    sleep(1)
+                    timeout_seconds -= 1
+        return result
+        
+    def is_checked(self, element: tuple[str, str] | WebElement, timeout_seconds: int | None = None) -> bool:
+        result: bool = False
+        if isinstance(element, tuple[str, str]):
+            element = self.driver.find_element(*element)
+        if isinstance(timeout_seconds, None):
+            result = element.is_selected()
+        else:
+            while timeout_seconds > 0:
+                result = element.is_selected()
+                if result:
+                    break
+                else:
+                    sleep(1)
+                    timeout_seconds -= 1
+        return result
+        
+    def is_element_displayed(self, element: tuple[str, str] | WebElement, timeout_seconds: int | None = None) -> bool:
+        result: bool = False
+        if isinstance(element, tuple[str, str]):
+            element = self.driver.find_element(*element)
+        if isinstance(timeout_seconds, None):
+            result = element.is_displayed()
+        else:
+            while timeout_seconds > 0:
+                result = element.is_displayed()
+                if result:
+                    break
+                else:
+                    sleep(1)
+                    timeout_seconds -= 1
+        return result
+    
+    def select_dropdown_by_text(self, element: tuple[str, str] | WebElement, text: str, log_message: str, timeout_seconds: int | None = None):
+        try:
+            self.wait_for_element_to_be_visible(element, timeout_seconds)
+            self.scroll_to_element(element)
+            self.wait_for_element_to_be_clickable(element, timeout_seconds)
+            if isinstance(element, tuple[str, str]):
+                element = self.driver.find_element(*element)
+            # Select class implementation
+        except Exception as e:
+            self.logger.exception(e)
+            raise f"Exception occured: {e}"
+
     def double_click(self, element: tuple[str, str] | WebElement, log_message: str, timeout_seconds: int | None = None):
         try:
             self.wait_for_element_to_be_visible(element, timeout_seconds)
@@ -94,6 +168,34 @@ class SeleniumBase:
                 element = self.driver.find_element(*element)
             ActionChains(self.driver).move_to_element(element).double_click().perform()
             self.logger.info(f"{log_message} | Double clicked on element | {element}")
+        except Exception as e:
+            self.logger.exception(e)
+            raise f"Exception occured: {e}"
+        
+    def click_and_hold(self, element: tuple[str, str] | WebElement, log_message: str, timeout_seconds: int | None = None):
+        try:
+            self.wait_for_element_to_be_visible(element, timeout_seconds)
+            self.scroll_to_element(element)
+            self.wait_for_element_to_be_clickable(element, timeout_seconds)
+            if isinstance(element, tuple[str, str]):
+                element = self.driver.find_element(*element)
+            ActionChains(self.driver).move_to_element(element).click_and_hold().perform()
+            self.logger.info(f"{log_message} | Click and holded on element | {element}")
+        except Exception as e:
+            self.logger.exception(e)
+            raise f"Exception occured: {e}"
+        
+    def drag_and_drop(self, source_element: tuple[str, str] | WebElement, destination_element: tuple[str, str] | WebElement, log_message: str, timeout_seconds: int | None = None):
+        try:
+            self.wait_for_element_to_be_visible(source_element, timeout_seconds)
+            self.wait_for_element_to_be_visible(destination_element, timeout_seconds)
+            self.scroll_to_element(source_element)
+            if isinstance(source_element, tuple[str, str]):
+                source_element = self.driver.find_element(*source_element)
+            if isinstance(destination_element, tuple[str, str]):
+                destination_element = self.driver.find_element(*destination_element)
+            ActionChains(self.driver).drag_and_drop(source_element, destination_element).perform()
+            self.logger.info(f"{log_message} | Dragged and dropped | {source_element}, {destination_element}")
         except Exception as e:
             self.logger.exception(e)
             raise f"Exception occured: {e}"

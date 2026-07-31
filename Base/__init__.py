09afb06b964e419f86d1c730d8fc580374da99f9
Seepana Swaminaidu <seepanaswaminaidu@gmail.com>
2026-07-31 14:16:59 +0530

    Used relative import in base package

--- Base/__init__.py
+++ Base/__init__.py
@@ -1,10 +1,10 @@
-from .browser import Browser
-from .selenium_base import SeleniumBase
-from .utilities import get_config_data,\
-                       get_logger, \
-                       generate_random_string, \
-                       generate_random_lower_string, \
-                       generate_random_upper_string, \
-                       generate_random_number, \
-                       generate_timestamp, \
-                       generate_timestamp_without_space
+from browser import Browser
+from selenium_base import SeleniumBase
+from utilities import get_config_data,\
+                      get_logger, \
+                      generate_random_string, \
+                      generate_random_lower_string, \
+                      generate_random_upper_string, \
+                      generate_random_number, \
+                      generate_timestamp, \
+                      generate_timestamp_without_space
